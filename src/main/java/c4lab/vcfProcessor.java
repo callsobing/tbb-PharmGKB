d8583c4c6b05dd04ac143432b788596dd0a4253d
yat <callsobing@gmail.com>
2016-11-17 18:05:15 +0800

    update application

--- src/main/java/c4lab/vcfProcessor.java
+++ src/main/java/c4lab/vcfProcessor.java
@@ -60,6 +60,8 @@ public class vcfProcessor implements Serializable {
                     }
                     Float allelFreq = chromosomeCount/sampleChromCount;
                     output.add(new Tuple2<String, String>(variantContext.getID(), allelFreq.toString()));
+                } else {
+                    output.add(new Tuple2<String, String>(variantContext.getID(), "not in the list"));
                 }
                 return output.iterator();
             }
